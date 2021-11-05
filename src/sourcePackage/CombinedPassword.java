2183dc30cc97819beda2cd722dc4bfe0a5934999
Jooyul Yoon <juyeolyoon@gmail.com>
2021-11-05 15:51:30 -0700

    getSalt method and combinedPassword class

--- src/sourcePackage/CombinedPassword.java
+++ src/sourcePackage/CombinedPassword.java
@@ -1,5 +1,53 @@
 package sourcePackage;
 
-public class CombinedPassword {
+import java.security.MessageDigest;
 
+public class CombinedPassword {
+  private String password;
+  private String salt;
+
+  public CombinedPassword(String password, String salt){
+    this.password = password;
+    this.salt = salt;
+  }
+
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getSalt() {
+    return this.salt;
+  }
+
+  public void setSalt(String salt) {
+    this.salt = salt;
+  }
+
+  public String getSecuredPassword() {
+    String hashedPassword;
+
+    try{
+      MessageDigest md = MessageDigest.getInstance("SHA-256");
+      md.update(salt.getBytes());
+      byte[] bytes = md.digest(password.getBytes());
+
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < bytes.length; i++) {
+        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+      }
+      hashedPassword = sb.toString();
+    
+      return hashedPassword;
+    } 
+    catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+    
+  }
 }
--- src/sourcePackage/Runner.java
+++ src/sourcePackage/Runner.java
@@ -1,5 +1,34 @@
 package sourcePackage;
 
-public class Runner {
+import java.security.SecureRandom;
 
+public class Runner {
+  public static void main(String args[]) 
+  {
+    String salt = getSalt();
+    System.out.println(salt);
+    CombinedPassword combinedPW = new CombinedPassword("password", salt);
+    System.out.print("password: " + combinedPW.getPassword());
+    System.out.println(" | salt: " + combinedPW.getSalt());
+    System.out.println("hashed password: " + combinedPW.getSecuredPassword());
+  }
+
+
+  private static String getSalt() 
+  {
+    SecureRandom random = new SecureRandom();
+    byte[] salt = new byte[16];
+    random.nextBytes(salt);
+    return Byte_To_String(salt);    
+  }
+
+
+  private static String Byte_To_String(byte[] temp) 
+  {
+    StringBuilder sb = new StringBuilder();
+    for(byte a: temp) {
+      sb.append(String.format("%02x", a));
+    }
+    return sb.toString();
+  }
 }
